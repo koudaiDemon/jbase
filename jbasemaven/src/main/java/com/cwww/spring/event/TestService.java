770e6337965e5c25f1841f5cca515470ed8c50de
蔡为 <wei.cai@hand-china.com>
2018-07-16 16:41:41 +0800

    创建第一个demo-代码迁移//todo 代码整理

---
+++ jbasemaven/src/main/java/com/cwww/spring/event/TestService.java
@@ -0,0 +1,32 @@
+package com.cwww.spring.event;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author cWww
+ * @Title
+ * @Description
+ * @date: 2018/7/2  14:28
+ */
+@Service
+public class TestService implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void onEvent() {
+        System.out.println("插入数据库");
+        TestEvent event = new TestEvent(this);
+        this.applicationContext.publishEvent(event);
+    }
+
+}
